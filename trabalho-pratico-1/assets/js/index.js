760cc710afcbc4d0a3f04ed68a1eb247b153f5ed
Christian Passold <passold.christian@hotmail.com>
2018-08-20 21:45:12 -0300

    wip

---
+++ trabalho-pratico-1/assets/js/index.js
@@ -0,0 +1,30 @@
+function activeMenu(element) {
+    let current = document.getElementsByClassName("active");
+
+    if (!!current[0]) {
+        current[0].className = current[0].className.split(/[^A-Za-z]/).filter(value => value !== 'active').join(" ");
+    }
+
+    const parent = element.parentElement
+
+    let arrClass = parent.className.split(/[^A-Za-z]/);
+
+    arrClass.push("active");
+
+    parent.className = arrClass.join(" ");
+}
+
+function validInput() {
+    let searchInput = document.getElementById("searchInput");
+
+    try {
+        if (!!searchInput.value && parseInt(searchInput.value, 10) > 3) {
+            alert('Busca realizada com sucesso')
+        } else {
+            alert('Preencha o campo de busca')
+        }
+    } catch (e) {
+        alert('Preencha o campo de busca')
+    }
+
+}
